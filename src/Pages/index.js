1f53642e83a2950c852b9ceaf14428ab39ccf8ab
meethatkar <meethatkar000@gmail.com>
2025-09-01 21:14:29 +0530

    added navbar

---
+++ src/Pages/index.js
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import About from './About.jsx'
+import Account from "./Account";
+import Cart from "./Cart";
+import Contact from "./Contact";
+import Like from "./Like";
+import Shop from "./Shop";
+
+export {About, Account, Cart, Contact, Like, Shop, Home };
+
+
